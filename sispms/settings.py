69b2fd1fc5757c479a8eb974a8253e84ae04b1d1
dnetochaves <dnetochaves@gmail.com>
2019-08-06 13:40:24 -0300

    Atualização Leyout

--- sispms/settings.py
+++ sispms/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['sispms.herokuapp.com']
-
+#ALLOWED_HOSTS = ['sispms.herokuapp.com']
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'usuario',
     'home',
+    'bootstrapform',
 ]
 
 MIDDLEWARE = [

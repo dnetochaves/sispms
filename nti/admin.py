7d3d7a2dc0221a458e25102f080be264c994a662
dnetochaves <dnetochaves@gmail.com>
2021-01-06 04:01:55 -0300

    Model NTI

--- nti/admin.py
+++ nti/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Equipamento, Tags
 
 # Register your models here.
+admin.site.register(Equipamento)
+admin.site.register(Tags)
--- nti/models.py
+++ nti/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from setor.models import Setor
 
-# Create your models here.
+
+class Tags(models.Model):
+    nome = models.CharField(max_length=50)
+    observacao = models.CharField(max_length=50)
+    setor_tag = models.ForeignKey(Setor, on_delete=models.CASCADE)
+
+
+class Equipamento(models.Model):
+    descricao = models.CharField(max_length=255)
+    fabricante = models.CharField(max_length=255, null=True, blank=True)
+    modelo = models.CharField(max_length=255, null=True, blank=True)
+    tombo = models.CharField(max_length=50, null=True, blank=True)
+    observacao = models.TextField(null=True, blank=True)
+    setor = models.ForeignKey(Setor, on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tags)
+
+    def __str__(self):
+        return self.descricao
